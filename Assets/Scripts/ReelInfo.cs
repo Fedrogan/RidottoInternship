45c4987097548c6ff3a65ddf0b126cd7803c5fd7
Fedar Hanutsin <gannutin@mail.ru>
2021-05-01 17:00:44 +0300

    try

--- Assets/Scripts/ReelInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-public class ReelInfo : MonoBehaviour
-{    
-    public bool isStopping;
-    public int reelId;
-}
--- Assets/Scripts/ScriptableObjects/FinalScreenSO.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Final Screen", menuName = "Final Screen")]
-public class FinalScreenSO : ScriptableObject
-{
-    [SerializeField] private List<SymbolData> finalScreenSymbols;
-    public List<SymbolData> FinalScreenSymbols  => finalScreenSymbols;
-}
