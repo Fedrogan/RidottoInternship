d78c44424e79111cd637d004aa1cecf4ef5d6a99
fedor.gannutin <fedor.gannutin@ridotto.by>
2021-06-05 00:34:37 +0300

    1. Particle animations added to winning symbols

--- Assets/Scripts/SymbolsManagement.cs
+++ Assets/Scripts/SymbolsManagement.cs
@@ -4,23 +4,18 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class SymbolsManagement : MonoBehaviour
-{
-    #region FIELDS
-    [SerializeField] private GameManagement gameManager;
+{    
     [SerializeField] private RectTransform mainCanvasRT;
     [SerializeField] private RectTransform[] symbols;
     [SerializeField] private List<SymbolData> symbolsData;
     [SerializeField] private List<FinalScreenSO> finalScreens;
     [SerializeField] private bool isRandomGame;
-    //[SerializeField] private List<WinLine> winLines;
 
-    //для проверки на каком риле находится символ
     [SerializeField] private List<RectTransform> reelAnchors;
 
     private readonly int reelSymbolsCount = 4;
     private readonly int defaultExitYPos = 240;
 
-    //для чтения финальных символов
     private int firstReelSymbol;
     private int secondReelSymbol;
     private int thirdReelSymbol;
@@ -32,16 +27,17 @@ public class SymbolsManagement : MonoBehaviour
     private float symbolHeigth;
     public bool isMutable;
     private float mainCanvasScale;
-    #endregion
 
     public bool IsRandomGame { get => isRandomGame; }
 
     void Start()
     {
+        GameController.Instance.SpinStarted += ResetSymbols;
         isMutable = true;
         currentSet = -1;
         mainCanvasScale = mainCanvasRT.lossyScale.y;
     }
+
     void Update()
     {
         for (int i = 0; i < symbols.Length; i++)
@@ -53,13 +49,14 @@ public class SymbolsManagement : MonoBehaviour
 
     public void ResetSymbols()
     {
+        if (!IsRandomGame) NextSet();
         firstReelSymbol = -3;
         secondReelSymbol = -2;
         thirdReelSymbol = -1;
     }
+
     public void NextSet()
     {
-        print("NextSet");
         _ = currentSet >= finalScreens.Count - 1 ? currentSet = 0 : currentSet += 1;
     }
 
@@ -70,26 +67,22 @@ public class SymbolsManagement : MonoBehaviour
             ChangeSymbolPosition(symbol);
         }
     }
+
     private void ChangeSymbolPosition(RectTransform symbol)
     {
         symbolHeigth = symbol.rect.height;
         var offset = symbol.position.y + symbolHeigth * mainCanvasScale * reelSymbolsCount;
         var newPos = new Vector3(symbol.position.x, offset, symbol.position.z);
         symbol.position = newPos;
-        if (isMutable) ChangeSpriteAndSetSymbolData(symbol);
+        if (isMutable && symbol.GetComponent<SlotSymbol>() != null) ChangeSpriteAndSetSymbolData(symbol);
     }
 
     private void ChangeSpriteAndSetSymbolData(RectTransform symbol)
     {
-        var newSymbol = symbol.GetComponentInParent<ReelInfo>().isStopping && !isRandomGame ?
+        var newSymbol = symbol.GetComponentInParent<ReelInfo>().IsStopping && !isRandomGame ?
             GetFinalScreenSymbol(symbol) : GetRandomSymbol();
         symbolData = newSymbol;
-        //Debug.Log(symbolData.SymbolID + " " + symbolData.SymbolName);
-        symbol.GetComponent<SlotSymbol>().SymbolSO = symbolData;
-        if (symbol.GetComponent<SlotSymbol>().DefaultParentReel == null)
-        {
-            symbol.GetComponent<SlotSymbol>().DefaultParentReel = (RectTransform)symbol.parent;
-        }        
+        symbol.GetComponent<SlotSymbol>().SymbolSO = symbolData;       
         symbolSprite = symbolData.SymbolImage;
         symbol.GetComponent<Image>().sprite = symbolSprite;
     }
@@ -97,10 +90,14 @@ public class SymbolsManagement : MonoBehaviour
     public void ResetSymbolsPosition(float correctedSlowDownDistance, float cellYCorrection, float startReelPositionY, RectTransform reel)
     {
         var symbolsCount = reel.childCount;
+        print("Symbols On Reel Count = " + symbolsCount);
         for (int i = 0; i < symbolsCount; i++)
         {
             var symbol = reel.GetChild(i);
+            print(symbol);
+            print("Symbol pos before = " + symbol.localPosition.y);
             var symbolPos = symbol.localPosition;
+            print(symbolPos);
             var newYPos = Mathf.Round(symbolPos.y + correctedSlowDownDistance - cellYCorrection - startReelPositionY);
             symbol.localPosition = new Vector3(symbolPos.x, newYPos, symbolPos.z);
         }
@@ -125,21 +122,22 @@ public class SymbolsManagement : MonoBehaviour
                 return 0;
         }
     }
+
     private int CheckSymbolReel(RectTransform symbol)
     {
         var currentReel =   symbol.IsChildOf(reelAnchors[0]) ? 1 :
                             symbol.IsChildOf(reelAnchors[1]) ? 2 :
                             symbol.IsChildOf(reelAnchors[2]) ? 3 : 0;
+
         return currentReel;
     }
+
     private SymbolData GetFinalScreenSymbol(RectTransform symbol)
     {
         var index = GetFinalSymbolIndex(symbol);
-        //if (index > 11)
-        //{
-        //    index = 0;
-        //}
+        index = index > finalScreens.Count - 1 ? 0 : index; 
         var newSymbol = finalScreens[currentSet].FinalScreenSymbols[index];
+        
         return newSymbol != null ? newSymbol : GetRandomSymbol();
     }
     private SymbolData GetRandomSymbol()
@@ -155,25 +153,13 @@ public class SymbolsManagement : MonoBehaviour
 
         for (int i = 0; i < childCount; i++)
         {
-            if (reel.GetChild(i).GetComponent<RectTransform>().localPosition.y != 0)
-                symbolsOnReel.Add(reel.GetChild(i).GetComponent<RectTransform>().GetComponent<SlotSymbol>());
+            var symbol = reel.GetChild(i).GetComponent<SlotSymbol>();
+            if (symbol != null && symbol.GetComponent<RectTransform>().localPosition.y != 0)
+                symbolsOnReel.Add(symbol);
         }
         var sortedSymbols = symbolsOnReel.OrderBy(x => x.GetComponent<RectTransform>().localPosition.y);
         var sortedSymbolsArray = sortedSymbols.ToArray();
         return sortedSymbolsArray[id];
-    }
-
-    public List<SlotSymbol> GetSymbolsOnReel(RectTransform reel)
-    {
-        var childCount = reel.childCount;
-        var symbolsOnReel = new List<SlotSymbol>();
-
-        for (int i = 0; i < childCount; i++)
-        {
-            symbolsOnReel.Add(reel.GetChild(i).GetComponent<RectTransform>().GetComponent<SlotSymbol>());
-        }
-        
-        return symbolsOnReel;
     }
 
     public SymbolData[] GetSymbolsData(SlotSymbol[] symbolsInLine)
@@ -197,8 +183,13 @@ public class SymbolsManagement : MonoBehaviour
             var childCount = reelAnchors[j].childCount;
             for (int k = 0; k < childCount; k++)
             {
-                allSymbols[i] = reelAnchors[j].GetChild(k).GetComponent<SlotSymbol>();
-                i++;
+                var symbol = reelAnchors[j].GetChild(k).GetComponent<SlotSymbol>();
+                print(symbol);
+                if (symbol != null)
+                {
+                    allSymbols[i] = symbol;
+                    i++;
+                }                
             }
         }
         return allSymbols;
